ab67e0320e145c316b80183d1ddbad3fb2af8719
Azhaghiri <ganadhan6528@gmail.com>
2024-11-13 22:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById("userform").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+
+
+let name = document.getElementById("name").value;
+let age = parseInt(document.getElementById("age").value);
+
+const ageVerify = new Promise((resolve, reject) =>{
+    setTimeout (() => {
+    if(age > 18){
+        resolve();
+    }
+    else{
+        reject();
+    }
+},4000);
+});
+
+ageVerify.then(() =>{
+    alert(`welcome ${name} you can vote`)
+}).catch(() =>{
+    alert(`Oh sorry ${name}. You aren't old enough.`)
+});
+});
+
+
+
